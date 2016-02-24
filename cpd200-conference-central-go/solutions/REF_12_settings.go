f67b7caa327e621f37a7b3a25dc372f8eaaf154b
Etienne Merceran <merceran.e@sfeir.com>
2016-02-24 15:16:34 +0100

    lab12

---
+++ cpd200-conference-central-go/solutions/REF_12_settings.go
@@ -0,0 +1,21 @@
+package main
+
+/*settings.go
+
+server-side Go App Engine app user settings
+
+$Id$
+
+*/
+
+import (
+
+)
+
+//Replace the following lines with client IDs obtained from the APIs
+//Console or Cloud Console.
+const (
+	WEB_CLIENT_ID = "replace with Web client ID"
+	ANDROID_CLIENT_ID = "replace with Android client ID"
+	ANDROID_AUDIENCE = WEB_CLIENT_ID
+)
